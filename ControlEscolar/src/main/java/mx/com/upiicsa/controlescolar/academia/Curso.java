dbfd2fa44cfca6535181e7a58281c72b80e9173d
Erick Avendaño <erick.avendano@igrtec.com>
2022-03-08 15:30:25 -0600

    Constructor Curso

--- ControlEscolar/src/main/java/mx/com/upiicsa/controlescolar/academia/Curso.java
+++ ControlEscolar/src/main/java/mx/com/upiicsa/controlescolar/academia/Curso.java
@@ -19,6 +19,7 @@ public class Curso {
     
     public Curso(){
         alumnos = new ArrayList<>();//Instancia de ArrayList
+        System.out.println("---------------->Constructor Curso");
     }
 
     public Materia getMateria() {
